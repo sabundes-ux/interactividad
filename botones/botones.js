f910f175708279a1a8133b95fe2478cf30726a56
sabundes-ux <sabundes@centro.edu.mx>
2026-04-16 12:15:44 -0600

    Add button styles and HTML structure for interactive buttons

---
+++ botones/botones.js
@@ -0,0 +1,7 @@
+let boton 1 = document.getElementById("btn-1");
+let boton 2 = document.getElementById("btn-2");
+let boton 3 = document.getElementById("btn-3");
+
+boton 1.addEventListener("click", function() {
+    console.log("Botón 1 presionado");
+}
--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ let c = a + b;
 // alert(c)
 console.log(c);
 
-function saludar (nombre="Mundo") {
+function saludar(nombre = "Mundo") {
     alert("Hola, ¿cómo estás?");
 }
 
 saludar("Chanchi");
 saludar("Bartolomeo");
 
-function sumar (a, b) {
+function sumar(a, b) {
     const resultado = a + b;
     alert("la suma de  " + a + " y " + b + " es: " + resultado);
 }
@@ -21,9 +21,9 @@ sumar(10, 20);
 
 function changeColor() {
     // camelCase
-    const r = Math.random()* 255;
-    const g = Math.random()* 255;
-    const b = Math.random()* 255;
+    const r = Math.random() * 255;
+    const g = Math.random() * 255;
+    const b = Math.random() * 255;
     document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
